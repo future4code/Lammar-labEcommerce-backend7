9256ee1d3b09be4150661bb059356f2a07c2a6d7
Caroline Marques <carolinemcs@hotmail.com>
2022-12-02 21:56:50 -0300

    Projeto Finalizado

--- revisao-mod5/src/endpoints/allCharactersAlphabeticOrder.ts
+++ revisao-mod5/src/endpoints/allCharactersAlphabeticOrder.ts
@@ -3,15 +3,23 @@ import connection from "../connection"
 import { character } from "../types";
 
 
-export default  async function getAllCharacters(
+export default  async function allCharactersAlphabeticOrder(
     req:Request,
     res:Response
 ): Promise<void> {
 
 try{
-   
+    const { name, orderBy, orderType, page } = req.query
+
+const resultsPerPage = 5
+
+const offset = resultsPerPage * (Number(page) - 1)
+
 const characters: character[] = await connection("character")
-    
+    .where("name", "LIKE", `%${name}%`)
+    .orderBy(orderBy as string || "name", orderType as string)
+    .offset(offset)
+
 res.send(characters)
     } catch (error){
       
